520a56beb1e3087268d6922df3872f6573de8173
Andrei <andrei.horodinca@gmail.com>
2015-03-05 11:32:15 +0100

    Edit build config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,11 +60,11 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.7"
         compile ":asset-pipeline:1.9.9"
         compile ":spring-security-core:2.0-RC4"
+        compile ":twitter-bootstrap:3.3.1"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
-        runtime ":resources:1.2.14"
         runtime ":cached-resources:1.0"
         runtime ":zipped-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
@@ -75,7 +75,7 @@ grails.project.dependency.resolution = {
         
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
-        //compile ":less-asset-pipeline:1.10.0"
+        compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
     }
